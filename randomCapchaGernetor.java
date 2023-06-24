44f939dc69a90b40cc2cce83cd3bfa62836e85ed
Sahil Kumar Gupta <100503629+sahilgupta0@users.noreply.github.com>
2023-06-24 16:00:35 +0530

    Delete randomCapchaGernetor.java

--- randomCapchaGernetor.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-import java.util.Random;
-
-public class randomCapchaGernetor {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-    }
-}
